c4055c0048ef2b84e49bc9fb7d1bbf791d204880
Cyber Square <70192724+cybersquare@users.noreply.github.com>
2020-10-23 07:49:41 +0530

    Add files via upload

---
+++ recap/dice.py
@@ -0,0 +1,7 @@
+# Write a random number generator that generates random numbers
+# between 1 and 6 (simulates a dice). 
+
+import random
+
+random_number = random.randint(1, 6)
+print(random_number)
